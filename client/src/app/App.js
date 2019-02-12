3f2702759567c2384da7d615fffdd6c8734acc1e
Carlos Rufo <sw@carlosrj.com>
2019-02-12 18:22:22 +0100

    refactor to js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,15 +1,16 @@
-import * as React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo-hooks';
 
 export default function App() {
   const {
     data: { launchesPast }
-  }: any = useQuery(query);
+  } = useQuery(query);
 
   return (
     <React.Fragment>
-      {launchesPast.map(({ mission_name, details, links }: any) => (
+      {launchesPast.map(({ mission_name, details, links }) => (
         <div key={String(mission_name)}>
           <h3>🛰 {mission_name}</h3>
           <p>{details}</p>
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import { Suspense } from 'react';
 import { ApolloProvider } from 'react-apollo-hooks';
-import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import App from './app/App';
-import registerServiceWorker from './utils/registerServiceWorker';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
@@ -16,7 +15,5 @@ ReactDOM.render(
       <App />
     </Suspense>
   </ApolloProvider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root')
 );
-
-registerServiceWorker();
