0a9f470e1185c091909c0144d71a5e337a475e41
Carlos Rufo <sw@carlosrj.com>
2019-02-19 13:08:24 +0100

    remove ships

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,31 +1,23 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo-hooks';
-import { GetLaunches } from '../types/types';
 import useFetch from 'fetch-suspense';
 
 export default function App() {
   const launchesPastRest = useFetch('http://localhost:4000/rest/launches-past');
   const {
     data: { launchesPast }
-  } = useQuery<GetLaunches.Query>(query);
+  } = useQuery(query);
 
   return (
     <React.Fragment>
-      {launchesPast.map(({ mission_name, details, links, rocket, ships }) => (
+      {launchesPast.map(({ mission_name, details, links, rocket }) => (
         <div key={String(mission_name)}>
           <h3>
             🛰 {mission_name} 🚀 {rocket.name}
           </h3>
           <p>{details}</p>
           <img src={links.flickr_images[0]} width="200" />
-          {!!ships.length && (
-            <React.Fragment>
-              <h4>⛴ {ships[0].name}</h4>
-              <p>{ships[0].port}</p>
-              <img src={ships[0].image} width="200" />
-            </React.Fragment>
-          )}
         </div>
       ))}
     </React.Fragment>
@@ -42,11 +34,6 @@ const query = gql`
       }
       rocket {
         name: rocket_name
-      }
-      ships {
-        name
-        port: home_port
-        image
       }
     }
   }
