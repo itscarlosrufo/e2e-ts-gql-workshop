2a2f3cb62799340355d898ced82b7dc76e7ca2f2
Carlos Rufo <sw@carlosrj.com>
2019-02-19 12:43:00 +0100

    remove graphql

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,46 +1,22 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-// import gql from 'graphql-tag';
-// import { useQuery } from 'react-apollo-hooks';
 import useFetch from 'fetch-suspense';
 
 export default function App() {
   const launchesPast = useFetch('https://api.spacex.land/rest/launches-past');
-  // const {
-  //   data: { launchesPastGraphQL }
-  // } = useQuery(query);
   console.log('launchesPast: ', launchesPast);
 
   return (
     <React.Fragment>
-      {launchesPast.map(({ mission_name, details, links, rocket, ships }) => (
+      {launchesPast.map(({ mission_name, details, links, rocket }) => (
         <div key={String(mission_name)}>
           <h3>
             🛰 {mission_name} 🚀 {rocket.rocket_name}
           </h3>
           <p>{details}</p>
           <img src={links.flickr_images[0]} width="200" />
-          {/* {!!ships.length && (
-            <React.Fragment>
-              <h4>⛴ {ships[0].name}</h4>
-              <p>{ships[0].port}</p>
-              <img src={ships[0].image} width="200" />
-            </React.Fragment>
-          )} */}
         </div>
       ))}
     </React.Fragment>
   );
 }
-
-// const query = gql`
-//   query getLaunches {
-//     launchesPast {
-//       mission_name
-//       details
-//       links {
-//         flickr_images
-//       }
-//     }
-//   }
-// `;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-// import { ApolloProvider } from 'react-apollo-hooks';
-// import ApolloClient from 'apollo-boost';
 const App = React.lazy(() => import('./app/App'));
 
-// const client = new ApolloClient({
-//   uri: 'https://api.spacex.land/graphql'
-// });
-
 ReactDOM.render(
-  // <ApolloProvider client={client}>
   <Suspense fallback="Loading...">
     <App />
   </Suspense>,
-  // </ApolloProvider>,
   document.getElementById('root')
 );
