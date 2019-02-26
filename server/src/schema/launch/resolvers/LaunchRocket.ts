673a9c9f27dc569efa9e58c294a6bdca8bcd19de
Carlos Rufo <sw@carlosrj.com>
2019-02-26 13:10:18 -0500

    add underfecthing logs

--- server/src/schema/launch/resolvers/LaunchRocket.ts
+++ server/src/schema/launch/resolvers/LaunchRocket.ts
@@ -1,8 +1,9 @@
 export const LaunchRocket = {
-  rocket: async ({ rocket_id }, args, context) => {
+  rocket: async (obj, args, context) => {
+    console.log('obj: ', obj);
     const [data] = await context.db
       .collection('rocket')
-      .find({ id: rocket_id })
+      .find({ id: obj.rocket_id })
       .limit(1)
       .toArray();
     return data;
